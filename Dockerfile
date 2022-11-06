5c9e4c2bd962fd7b648e075c24398e4bff8389d8
Guilherme Jr <falecom@guilhermejr.net>
2022-11-06 10:50:31 -0300

    release inicial

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM openjdk:11
+LABEL maintainer="Guilherme Jr. <falecom@guilhermejr.net>"
+ENV TZ=America/Bahia
+ARG VAULT_HOST
+ARG VAULT_TOKEN
+ENV VAULT_HOST=${VAULT_HOST}
+ENV VAULT_TOKEN=${VAULT_TOKEN}
+COPY sistema-gateway-server.jar sistema-gateway-server.jar
+ENTRYPOINT ["java","-jar","/sistema-gateway-server.jar"]
+EXPOSE 8080
---
+++ src/main/java/net/guilhermejr/sistema/gatewayserver/GatewayServerApplication.java
@@ -0,0 +1,17 @@
+package net.guilhermejr.sistema.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+}
